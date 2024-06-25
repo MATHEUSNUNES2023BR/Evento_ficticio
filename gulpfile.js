7d96b3e91cb75de2c61a376c5c8007fe75c06346
Matheus Nunes <devmatheusnunesdebarros@gmail.com>
2024-06-24 22:59:48 -0300

    Configurações iniciais

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const log = require('fancy-log');
+
+function buildStyles() {
+    log('Compilando arquivos SCSS...');
+    return gulp.src('./src/styles/*.scss')
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(gulp.dest('./dist/css'))
+        .on('end', () => log('Compilação completa! Arquivos gerados em dist/css'));
+}
+
+exports.default = buildStyles;
+function watch() {
+    gulp.watch('./src/styles/**/*.scss', buildStyles);
+}
+
+exports.watch = watch;
